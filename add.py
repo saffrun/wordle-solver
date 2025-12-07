48f93642e0afb44a18411f3133966841f982a985
uzayryas <112652936+uzayryas@users.noreply.github.com>
2025-12-06 22:53:43 -0500

    added prompting and input for CLI

--- add.py
+++ add.py
@@ -1,65 +1,3 @@
-import math 
-import pandas as pd
-
-
-CSV = 'wordle.csv'
-starter_words = ["slate", "crane", "adieu", "roate", "soare"]
-
-
-
-def weighted_answers(CSV: str):
-    df = pd.read_csv(CSV, header=0, usecols= [0,1], names=["word", "occurrence"])
-    #h = df.head()
-    #print(h)
-    # cleaning data
-    df["word"] = df["word"].astype(str).str.strip().str.lower()
-    #change occurence to #
-    df["occurrence"] = pd.to_numeric(df["occurrence"])
-    
-    # Remove Nas and non-positive probabilities
-    df = df.dropna(subset=["occurrence"])
-    df = df[df["occurrence"] > 0]
-
-    # Normalize into probabilities
-    total = df["occurrence"].sum()
-    df["prob"] = df["occurrence"] / total
-    #create lists
-    word_list = df["word"].tolist()
-    prob_list = df["prob"].tolist()
-
-    return word_list, prob_list
-
-
-
-
-
-def get_feedback(guess: str, answer: str):
-    #get everything to lower case 
-    guess = guess.lower()
-    answer = answer.lower()
-
-    gs343 = ["w" , "w" , "w" , "w" , "w"]
-    #inital string is set w(gray)
-    answer_chars = list(answer)
-
-    # check for green
-    for i in range(5):
-        if guess[i] == answer[i]:
-            gs343[i] = "g"
-            answer_chars[i] = None
-
-    # check for yellow 
-    for i in range(5):
-        if gs343[i] == "w":
-            if guess[i] in answer_chars:
-                gs343[i] = "y"
-                x = answer_chars.index(guess[i])
-                answer_chars[x] = None
-    #join the list to string and return it
-    return "".join(gs343)
-
-
-
 def main():
     answers, probs = weighted_answers(CSV)
 
@@ -99,5 +37,5 @@ def main():
             continue
 
 
-if __name__ == "__main__":
-    main()
+        print(f"Got guess {guess.upper()} with pattern {pattern}.")
+        print("Filtering logic coming soon... (placeholder)")
